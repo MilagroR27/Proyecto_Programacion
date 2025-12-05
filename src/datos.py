078d4f8cd2a3ed5eb25d160cea5229c07af5f787
MilagroR27 <rodriguezmm05@gmail.com>
2025-12-04 23:49:48 -0300

    me olvide los comentarios

--- src/datos.py
+++ src/datos.py
@@ -1,6 +1,10 @@
-import json #importamos el json
+import json  # Importa el módulo para trabajar con archivos JSON
 
 def cargar_pokedex():
-    with open("./data/pokemon.json", "r", encoding="utf-8") as archivo:
+# Abre el archivo 'pokemon.json' ubicado dentro de la carpeta 'data' en modo lectura ("r")
+# encoding="utf-8" asegura que se lean correctamente caracteres especiales como tildes o ñ
+    with open("./data/pokemon.json", "r", encoding="utf-8") as archivo:    
+# Convierte el contenido del archivo JSON a estructuras de Python (listas y diccionarios)
         data = json.load(archivo)
+# Del diccionario principal, devuelve solo la lista de pokémon asociada a la clave "pokemon"
     return data["pokemon"]
