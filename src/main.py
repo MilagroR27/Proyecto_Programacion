f2b66746824e1cc5fcd576ac0d57787bda95dc32
MilagroR27 <rodriguezmm05@hotmail.com>
2025-12-06 19:14:18 -0300

    if de prueba

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from datos import cargar_pokedex
+from datos import cargar_pokedex, tipo
 
 pokedex = cargar_pokedex()
 
@@ -6,3 +6,9 @@ print("Total de Pokémon cargados:", len(pokedex))
 print(pokedex[0])
 print("Primer Pokémon:", pokedex[0]["name"])
 print("Tipos:", pokedex[0]["type"])
+
+for pokemon in pokedex[:5]:
+    if tipo(pokemon, "Water"):
+        print(pokemon["name"], "es de tipo Agua")
+    else:
+        print(pokemon["name"], "NO es de tipo Agua")
